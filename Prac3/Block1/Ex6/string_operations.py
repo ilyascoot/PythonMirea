da21f6526351c4ab3cf9e73ef2efefd13ef70e40
Ilya <aylidota@gmail.com>
2023-03-27 20:31:27 +0300

    Block1

--- Prac3/Block1/Ex6/string_operations.py
+++
@@ -1,5 +0,0 @@
-def reverse_string(string):
-    return string[::-1]
-
-def count_words(string):
-    return len(string.split())
--- Prac3/Block1/main.py
+++
@@ -1,2 +0,0 @@
-from my_package import *
-print(math_operations.add(2, 5))
